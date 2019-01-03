573125b178a943d41a791601cbfe11467523f604
Nick Manton <nick@mantonbradbury.com>
2019-01-03 14:15:10 +0000

    add match count to teams index page

--- models/teams.js
+++ models/teams.js
@@ -27,6 +27,48 @@ const Team = (sequelize, DataTypes) => {
     }
   });
 
+  model.getMatches = async () => {
+
+    const models = require('.'),
+          Op     = require('sequelize').Op,
+          teams  = {};
+
+    try {
+      const matches = await models.Match.findAll({
+        where: { result: { [Op.ne]: null } },
+        attributes: [],
+        include: [{
+          model: models.Team,
+          as: 'TeamA',
+          attributes: ['id', 'name', 'country']
+        }, {
+          model: models.Team,
+          as: 'TeamB',
+          attributes: ['id', 'name', 'country']
+        }]
+      });
+      matches.map(match => {
+        if (teams[match.TeamA.name] == undefined) {
+          teams[match.TeamA.name] = { matches: 1, id: match.TeamA.id, country: match.TeamA.country };
+        } else {
+          teams[match.TeamA.name].matches++;
+        }
+        if (teams[match.TeamB.name] == undefined) {
+          teams[match.TeamB.name] = { matches: 1, id: match.TeamB.id, country: match.TeamB.country };
+        } else {
+          teams[match.TeamB.name].matches++;
+        }
+      });
+      return teams;
+
+    } catch (e) {
+
+      return e;
+
+    }
+
+  };
+
   return model;
 
 };
